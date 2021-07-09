d44af33fef82ece31864cce4b2833f8f29a3f4af
Akhilesh Sharma <55455350+aboutakhilesh@users.noreply.github.com>
2021-07-09 23:23:25 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,2 +1,12 @@
 <?php
+
+$user = $_POST['un'];
+$pass = $_POST['psaa'];
+
+$result = QUERY: SELECT * FROM users WHERE username='$user' AND password='$pass'
+  if( $result IS NOT BLANK ) {
+    echo "login successful";  }
+  else {
+    echo "login failed";  }
+
 ?>
