f8557ad6a73a61fbef5fe61e91ddc9c0e253a56e
akhil3sh555 <87195933+akhil3sh555@users.noreply.github.com>
2021-07-18 23:35:26 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,30 @@
 <?php
 
-$user = $_POST['un'];
-$pass = $_POST['psaa'];
+$host="host.address";
+$user="username";
+$password="";
+$db="dummy";
 
-$result = QUERY: SELECT * FROM users WHERE username='$user' AND password='$pass'
-  if( $result IS NOT BLANK ) {
-    echo "login successful";  }
+mysql_connect($host,$user,$password);
+mysql_select_db($db);
+
+if(isset($_POST['username'])) {
+  $username=$_POST['username'];
+  $password=$_POST['password'];
+  
+  $sql="select * from loginformnameddb where user='".$username."' and pass='".$password."' limit 1";
+  $result=mysql_query($sql);
+  
+  if(mysql_num_row($result)==1) {
+    echo " You have successfully logged in ";
+    exit();
+  }
+  
   else {
-    echo "login failed";  }
+    echo "You have Entered Incorrect Password";
+    exit();
+  }
+  
+}
 
 ?>
