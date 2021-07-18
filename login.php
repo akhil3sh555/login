b0c3aeb4d94706afd486da84da3f1c24d960bc4a
akhil3sh555 <87195933+akhil3sh555@users.noreply.github.com>
2021-07-18 23:53:48 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,9 @@
 <?php
 
 $host="host.address";
-$user="username";
+$user="3426248_dummy";
 $password="";
-$db="dummy";
+$db="3426248_dummy";
 
 mysql_connect($host,$user,$password);
 mysql_select_db($db);
@@ -12,7 +12,7 @@ if(isset($_POST['username'])) {
   $username=$_POST['username'];
   $password=$_POST['password'];
   
-  $sql="select * from loginformnameddb where user='".$username."' and pass='".$password."' limit 1";
+  $sql="select * from loginformdb where user='".$username."' and pass='".$password."' limit 1";
   $result=mysql_query($sql);
   
   if(mysql_num_row($result)==1) {
